da3e92f2670fa04f0c2b4ea40162d00703b1c178
fakhrul <fakhrul@mpower-social.com>
2024-11-29 10:00:55 +0600

    write test case for average temperature api

--- Weather/tests.py
+++ Weather/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.test import APIClient
 
-# Create your tests here.
+class AverageTemperatureApiTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+        refresh = RefreshToken.for_user(self.user)
+        self.token = str(refresh.access_token)
+
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        self.api_url = '/api/avg-temperature'
+
+    def test_average_temperature_api(self):
+        response = self.client.get(self.api_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('data', response.json())
+        self.assertEqual(len(response.json()['data']), 10)
